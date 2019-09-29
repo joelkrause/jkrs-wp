a7925b6e70f3dcc88bb8cb313ecb9f7ba2e022fd
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2019-09-29 11:23:11 +1000

    gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const del = require('del');
+
+gulp.task('prefix', () =>
+    gulp.src('lib/styles/css/main.css')
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 99 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('lib/styles/css'))
+);
 
 gulp.task('styles', () => {
     return gulp.src('lib/styles/scss/main.scss')
@@ -18,6 +28,6 @@ gulp.task('default', gulp.series(['clean', 'styles']));
 
 gulp.task('watch', () => {
     gulp.watch('lib/styles/scss/**/*.scss', (done) => {
-        gulp.series(['clean', 'styles'])(done);
+        gulp.series(['clean', 'styles', 'prefix'])(done);
     });
 });
