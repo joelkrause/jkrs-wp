6935ce8342e3dc90c0f4b0d1cc1a1f8075b86638
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2019-10-01 20:49:30 +1000

    Updated theme

--- footer.php
+++ footer.php
@@ -1,9 +1,15 @@
 <footer class="site__footer">
     <div class="wrapper">
-        &copy; Joel Krause | <a href="https://github.com/joelkrause/jkrs-wp" target="_blank">See on GitHub</a>
+        <div class="social">
+            <a href="https://twitter.com/Joel_Krause" target="_blank" class="twitter"><i class="fab fa-twitter"></i></a>
+            <a href="https://github.com/joelkrause" target="_blank" class="github"><i class="fab fa-github"></i></a>
+            <a href="https://linkedin.com/joel-krause" target="_blank" class="linkedin"><i class="fab fa-linkedin"></i></a>
+        </div>
+        <div class="credits">&copy; Joel Krause | <a href="https://github.com/joelkrause/jkrs-wp" target="_blank">View source on GitHub</a></div>
     </div>
 </footer>
 <?php wp_footer();?>
+<script src="//instant.page/2.0.0" type="module" defer integrity="sha384-D7B5eODAUd397+f4zNFAVlnDNDtO1ppV8rPnfygILQXhqu3cUndgHvlcJR2Bhig8"></script>
 </body>
 
 </html>
