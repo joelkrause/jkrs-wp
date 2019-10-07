5364442ff3e1e3f1e4bf5a65f207d1d06d2b7a1c
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2019-10-07 21:48:35 +1100

    Updated post icon field in acf

--- template-parts/post-card.php
+++ template-parts/post-card.php
@@ -12,7 +12,7 @@ $icon_color = get_field('icon_color','category_'.$category_id);
 ?>
 <div class="post__card" data-animate>
     <a href="<?php echo $link;?>">
-        <div class="post__card-icon" style="background-color:<?php echo $icon_color;?>;"><?php echo file_get_contents($icon['url']);?></div>
+        <div class="post__card-icon" style="background-color:<?php echo $icon_color;?>;"><?php echo $icon;?></div>
         <div class="post__card-title"><?php echo $title;?></div>
         <div class="post__card-date"><?php echo $date;?></div>
     </a>
